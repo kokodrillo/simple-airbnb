e8e94e0f271ba4dba18655b30ed30fab19c473db
Maja Polanc <mayainstudio@gmail.com>
2020-02-29 20:25:35 +0100

    added actions for controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,23 @@
 class FlatsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
 end
