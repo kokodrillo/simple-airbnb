aacd6ae8e610ab6ce8357edf814cad49dd335c69
Maja Polanc <mayainstudio@gmail.com>
2020-03-07 21:23:48 +0100

    changed grid and a card structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,18 +36,3 @@ flat4 = Flat.create!(
   price_per_night: 75,
   number_of_guests: 3
 )
-
-flat5 = Flat.create!(
-  name: 'Light & Spacious Garden Flat London',
-  address: '10 Clifton Gardens London W9 1DT',
-  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  price_per_night: 75,
-  number_of_guests: 3
-)
-flat6 = Flat.create!(
-  name: 'Light & Spacious Garden Flat London',
-  address: '10 Clifton Gardens London W9 1DT',
-  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  price_per_night: 75,
-  number_of_guests: 3
-)
